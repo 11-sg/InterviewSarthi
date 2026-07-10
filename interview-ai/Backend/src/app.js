b72f6158c24b97fe0603089d1a5cfc9a8140fee2
11-sg <sg4242674@gmail.com>
2026-07-11 01:37:39 +0530

    Render deployment changes

--- interview-ai/Backend/src/app.js
+++ interview-ai/Backend/src/app.js
@@ -7,8 +7,11 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+   origin: [
+  "http://localhost:5173",
+  "https://interviewsarthi.onrender.com/"
+],
+credentials: true
 }))
 
 /* require all the routes here */
